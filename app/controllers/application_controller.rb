0f0a2c2bf997e324ac44e3ebe1d2bba8e881290c
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-02-24 22:03:04 -0600

    configure do AppCon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,12 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    #set sessions
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
+    #register Sinatra::Flash
   end
 
   get "/" do
