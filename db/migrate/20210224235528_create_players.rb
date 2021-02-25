3040dce0226598dff6c5ebd5953a40b378d4a78a
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-02-24 18:35:42 -0600

    player table db

---
+++ db/migrate/20210224235528_create_players.rb
@@ -0,0 +1,10 @@
+class CreatePlayers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :players do |t|
+      t.string :username
+      t.string :email
+      t.string :password
+      t.integer :team_id
+      t.integer :total_solved
+  end
+end
