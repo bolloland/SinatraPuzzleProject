29ceab541504de6b5081c6543d50b75b83846866
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-02-24 22:27:03 -0600

    rake pry console

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
+    #binding.pry
     erb :welcome
   end
 
---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,8 @@
+class PlayersController < ApplicationController
+    #for anything pertaining to players
+
+    # get '/register' do
+    #     erb :'players/register' do
+    # end
+
+end
