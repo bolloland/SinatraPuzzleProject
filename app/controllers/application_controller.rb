311db3e201d13e24429bb51462f3950be39383bd
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-03-01 12:10:28 -0600

    post /signup' do in AppCon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,34 @@ class ApplicationController < Sinatra::Base
    erb :"/players/signup"
   end
 
-  
+  post '/signup' do
+    binding.pry
+        @player = Player.create(params)
+        #validate new player:
+        if player.username.blank? || player.email.blank? || player.password.blank? || Player.find_by_email(params[:email]) || Player.find_by_username(params[:username])
+            redirect '/signup'
+        else 
+            player.save
+            session[:player_id] = player.id # logging user in
+            redirect '/welcome'
+        end 
+    end 
+
+  post "/login" do
+    # binding.pry
+    @player = Player.find_by_username(params[:username])
+
+      if @player == nil
+        redirect "/"
+        # flash[:error] = "Invalid username or password"
+      elsif !!@player.authenticate(params[:password])
+        session[:id] = @player.authenticate(params[:password]).id
+        # session[:team_id] = team.id   
+        redirect "/teams/#{session[:team_id]}"
+      else
+        redirect to '/'
+      end
+  end
   
   helpers do #LINK controllers and views
 
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,21 +1,21 @@
 class PlayersController < ApplicationController
     #for anything pertaining to players
 
-    post "/login" do
-        # binding.pry
-        @player = Player.find_by_username(params[:username])
+    # post "/login" do
+    #     # binding.pry
+    #     @player = Player.find_by_username(params[:username])
     
-          if @player == nil
-            redirect "/"
-            #FLASH ERROR IN HERE?
-          elsif !!@player.authenticate(params[:password])
-            session[:id] = @player.authenticate(params[:password]).id
-            # session[:team_id] = team.id   
-            redirect "/teams/#{session[:team_id]}"
-          else
-            redirect to '/'
-          end
-      end
+    #       if @player == nil
+    #         redirect "/"
+    #         #FLASH ERROR IN HERE?
+    #       elsif !!@player.authenticate(params[:password])
+    #         session[:id] = @player.authenticate(params[:password]).id
+    #         # session[:team_id] = team.id   
+    #         redirect "/teams/#{session[:team_id]}"
+    #       else
+    #         redirect to '/'
+    #       end
+    #   end
      
 
     get "/welcome/:id" do
