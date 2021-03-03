1165aa4c0faf4b9def63fbcdb4651162f8f60a5b
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-03-02 18:06:39 -0600

    New Puzzle Create

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -1,7 +1,33 @@
 class PuzzlesController < ApplicationController
 
-    get '/upload' do
-        erb :"/puzzles/upload"
+    get '/puzzles/new' do
+        current_player
+        erb :"/puzzles/new"
     end
+
+    post "/puzzles" do
+        current_player
+        @current_player.games
+        new_puz = Puzzle.create(params)
+        Game.create(player_id: session[:player_id], puzzle_id: new_puz.id)
+
+        redirect "puzzles/new_edit/#{session[:player_id]}"
+    end
+
+    get "puzzles/new_edit/:id" do
+        @player = Player.find_by_id(params[:id])
+        current_player
+#   binding.pry
+        erb :"puzzles/new_edit"
+end
+
+    # get '/puzzles/upload/:id' do
+    #     binding.pry
+    #     @player = Player.find_by_id(params[:id])
+    #     session[:player_id] = player.id
+    #     erb :"/puzzles/upload/:id"
+    # end
    
+
+    
 end
