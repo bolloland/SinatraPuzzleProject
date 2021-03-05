0c008ba1e64ad466ea84cf909078fe7d63756873
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-03-04 21:55:10 -0600

    post /puzzles error messages

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -13,35 +13,33 @@ class PuzzlesController < ApplicationController
 
     get "/puzzles/:id" do
         get_puzzle
+        current_player
+        # binding.pry
         erb :"/puzzles/show"
     end
 
     post "/puzzles" do
-        current_player
-        @current_player.games
-        new_puz = Puzzle.create(params)
-        Game.create(player_id: session[:player_id], puzzle_id: new_puz.id)
-        
-
-        redirect "/puzzles/#{session[:player_id]}"
+        puzzle = Puzzle.new(params)
+        if puzzle.title.blank? || puzzle.description.blank? || puzzle.category.blank? || puzzle.solution.blank? 
+            flash[:error] = "You left something blank!"
+            redirect '/puzzles/new'
+        elsif
+            Puzzle.exists?(title:params[:title])
+            # puzzle.title.exists?
+            flash[:error] = "Sorry, that title already exists :("
+            redirect '/puzzles/new'
+            binding.pry
+        else
+            current_player
+            @current_player.games
+            # new_puz = Puzzle.new(params)
+            # Game.create(player_id: session[:player_id], puzzle_id: new_puz.id)
+            # new_puz.save
+            Game.create(player_id: session[:player_id], puzzle_id: puzzle.id)
+            puzzle.save
+            redirect "/puzzles/#{session[:player_id]}"
+        end          
     end
-
-    # redirect "/puzzles/edit/#{session[:player_id]}"
-
-    # if puzzle.title == "" || puzzle.description == "" || puzzle.category == "" || puzzle.solution == "" 
-    #     # flash[:alert] = "You left something blank!"
-    #     redirect '/puzzles/new'
-    # else 
-    #     current_player
-    #     @current_player.games
-    #     new_puz = Puzzle.create(params)
-    #     Game.create(player_id: session[:player_id], puzzle_id: new_puz.id)
-    # #   ##
-    # #     puzzle.save
-    # #     session[:player_id] = player.id # logging user in
-    #     redirect "/puzzles/edit/#{session[:player_id]}"
-    # end   
-
 
     get "/puzzles/:id/edit" do
         get_puzzle
